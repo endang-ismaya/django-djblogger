54a7ffa56290ed1254fffd6003a6d2ce889bf2a2
Endang Ismaya <endang.ismaya@gmail.com>
2023-06-13 17:44:03 +0700

    update cr_app.sh with APP_NAME variable

--- cr_app.sh
+++ cr_app.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
+# Set Variable
+APP_NAME=$1
+
 # create app directory
-mkdir apps/$1
+mkdir apps/${APP_NAME}
 
 # run startapp
-python manage.py startapp apps_$1 apps/$1
+python manage.py startapp apps_${APP_NAME} apps/${APP_NAME}
 sleep 1
 
 # cr urls.py
-touch apps/$1/urls.py
-echo "from django.urls import path" >> apps/$1/urls.py
-echo "" >> apps/$1/urls.py
-echo "urlpatterns = [" >> apps/$1/urls.py
-echo "  path(\"\")," >> apps/$1/urls.py
-echo "]" >> apps/$1/urls.py
+touch apps/${APP_NAME}/urls.py
+echo "from django.urls import path" >> apps/${APP_NAME}/urls.py
+echo "" >> apps/${APP_NAME}/urls.py
+echo "urlpatterns = [" >> apps/${APP_NAME}/urls.py
+echo "  path(\"\")," >> apps/${APP_NAME}/urls.py
+echo "]" >> apps/${APP_NAME}/urls.py
 sleep 1
 
 # cr templates app
-mkdir templates/$1
+mkdir templates/${APP_NAME}
 sleep 1
 
 # cr forms.py
-touch apps/$1/forms.py
-echo "from django import forms" >> apps/$1/forms.py
+touch apps/${APP_NAME}/forms.py
+echo "from django import forms" >> apps/${APP_NAME}/forms.py
 sleep 1
 
 # cr views.py
-rm apps/$1/views.py
-touch apps/$1/views.py
-echo "from django.views import View" >> apps/$1/views.py
-echo "from django.shortcuts import render" >> apps/$1/views.py
+rm apps/${APP_NAME}/views.py
+touch apps/${APP_NAME}/views.py
+echo "from django.views import View" >> apps/${APP_NAME}/views.py
+echo "from django.shortcuts import render" >> apps/${APP_NAME}/views.py
 
 # update apps_ to apps.
-sed -i "s/apps_$1/apps.$1/gi" apps/$1/apps.py
+sed -i "s/apps_${APP_NAME}/apps.${APP_NAME}/gi" apps/${APP_NAME}/apps.py
 
 # add installed apps
-sed -i '/internal apps/a\ \ \ \ "apps.$1",' _project/settings.py
+sed -i "/internal apps/a\ \ \ \ \"apps.${APP_NAME}\"," _project/settings.py
